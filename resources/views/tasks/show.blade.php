1d1b48fbc135c99f58915238eecbe613ed50f60a
hitomi.sakamoto <omeme103@gmail.com>
2020-08-19 14:11:49 +0000

    tasklist complete

---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>id= {{ $task->id }}のタスク詳細ページ</h1>
+    
+    <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <th>{{ $task->id }}</th>
+        </tr>
+        <tr>
+            <th>タスク</th>
+            <td>{{ $task->content }}</td>
+        </tr>
+    </table>
+    
+    {!! link_to_route('tasks.edit', 'このタスクを編集', ['task' => $task->id], ['class' => 'btn btn-light']) !!}
+
+    {!! Form::model('tasks.destroy', ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
+    {!! Form::close() !!}
+@endsection
